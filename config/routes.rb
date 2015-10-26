0c3b639ef70ce7b9c400c5cdbe0e2d7d4a3cc2d5
Mvittini <mvittini@live.cl>
2015-10-26 04:40:29 -0300

    install devise, created devise views and user, migrated, count comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'reviews/create'
 
   resources :products do
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'product#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
