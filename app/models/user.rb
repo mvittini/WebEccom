6bbfb9f6e1a08a5d2060027a35fce1abe73991ca
Mvittini <mvittini@live.cl>
2015-10-26 05:49:31 -0300

    added reference review to user n migrated

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :review, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :lastname, presence: true
+  validates :username, presence: true
+end
