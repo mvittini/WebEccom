641e87bf38d81ca999936ff2e79a041c91d87799
Mvittini <mvittini@live.cl>
2015-10-26 03:08:34 -0300

    scaffold product, model reviews and their relations + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy
+Review.destroy
+
+products = []
+
+(1..50).each do |m|
+  products << Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(2, true, 10),
+    price: Faker::Number.between(100, 100000),
+    stock: Faker::Number.between(1, 99)
+  )
+end
+
+reviews = []
+
+(1..300).each do |r|
+  reviews << Review.create(
+    content: Faker::Lorem.paragraph(3),
+    product: products[rand(products.length)]
+  )
+end
