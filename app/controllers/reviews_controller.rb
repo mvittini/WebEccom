2df2801559fcb5ec8508b1dda32538bac6cf3bfe
Mvittini <mvittini@live.cl>
2015-10-26 03:58:04 -0300

    routes for review, form for review inside product, reballing form

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+  def create
+  	@product = Product.find(params[:product_id])
+  	@review = @product.reviews.build(reviews_params)
+  	@review.save
+  	if @review.save
+  		redirect_to @product, notice: "el comentario fue ingresado"
+  	else
+  		redirect_to @product, notice: "el comentario no fue ingresado"
+  	end
+  end
+  def reviews_params
+  	params.require(:review).permit(:content)
+  end
+end
