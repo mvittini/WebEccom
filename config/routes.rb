64461498e369d8005b08960a317fa73bf64d5d51
Mvittini <mvittini@live.cl>
2015-10-29 13:32:42 -0300

    added menu of Categories in Index(not funcional, only list), show column category in Index, show category of product on Show, added ComboBox of category to new product(not funcional, only list)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get 'darlike'
     end
   end
+  resources :categories, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
